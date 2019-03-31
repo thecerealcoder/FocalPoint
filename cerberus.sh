58b1e0bd581f1054b5573f7af23d68968fbc3125
Tom Biju <tom.biju@4cinsights.com>
2019-03-31 18:44:01 -0500

    mac alternative blocking

--- cerberus.sh
+++ cerberus.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-if [ -s apps.txt ]
-then
-     echo "File not empty"
-else
-     ls /Applications > apps.txt
-fi
-input="apps.txt"
-while IFS= read -r var
+# if [ -s apps.txt ]
+# then
+#      echo "File not empty"
+# else
+#      ls /Applications > apps.txt
+# fi
+# input="apps.txt"
+# while IFS= read -r var
+# do
+#   sudo spctl --add --label "ApprovedApps" /Applications/"$var"
+# done < "$input"
+# sudo spctl --enable --label "ApprovedApps" 
+# sudo spctl --add --label "DeniedApps" /Applications/Firefox.app
+# sudo spctl --disable --label "DeniedApps" 
+while true
 do
-  sudo spctl --add --label "ApprovedApps" /Applications/"$var"
-done < "$input"
-sudo spctl --enable --label "ApprovedApps" 
-sudo spctl --add --label "DeniedApps" /Applications/Firefox.app
-sudo spctl --disable --label "DeniedApps" 
-
+     array=( "Google Chrome" "Notes" "firefox" )
+     for program in "${array[@]}"
+     do
+          if pgrep "$program"
+               then
+                    targets=($(pgrep "$program"))
+                    for i in "${targets[@]}"
+                    do
+                         kill $i
+                    done
+          fi  
+     done
+     sleep 1
+done
