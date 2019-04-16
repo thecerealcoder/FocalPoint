846c8ff1f3adf273ae41701bd96ae39f57bd1e99
thecerealcoder <thecerealcoder@gmail.com>
2019-04-16 15:27:14 -0700

    added unblocking feature for windows version

--- windows.js
+++ windows.js
@@ -56,25 +56,69 @@ function createReg(path) {
 //Create DisallowRun Value in Explorer Key
 function setKeyVal(reg) {
 	reg.set("DisallowRun","REG_DWORD","1", function (err){
-	if (err) console.log('ERROR: '+err);
+	if (err) console.log(err);
 });
 }
 
 //Prompt user input for programs to block and then create them as Disallow Key Values
 function recursiveRead(reg) {
 	var count = 0;
-	reg.values(function (err, items) {
-		if (err) console.log('ERROR: '+err);
-		count = items.length;
-		count++;
-	})
-
-	readline.question("Please enter the name of a program you want to block, including the .exe extension. Enter 0 to exit.\n", (name) => {
-		if(name == "0") return readline.close();
-		reg.set(count,"REG_SZ",name, function (err){
-			if (err) console.log('ERROR: '+err);
+		reg.values(function (err, items) {
+			if (err) console.log(err);
+			if(items == undefined) { 
+				count = 0;
+			} else {
+				count = items.length;
+				count++;
+			}
 		});
-		recursiveRead(reg);
-	})
+
+	readline.question("Enter 1 to block a program or 2 to unblock a program. Enter 0 to exit.\n", (option) => {
+		if(option == "0") {
+			return readline.close();
+		} else if (option == "1" || option == "2") {
+			appPermissions(reg, count, option);
+		} else {
+			console.log("Please enter a valid input.");
+			recursiveRead(reg);
+		}
+	});
 };
+
+//Lets user choose whether to block or unblock program
+function appPermissions (reg, count, option) {
+	if(option == "1"){
+		readline.question("Please enter the name of a program you want to block, including the .exe extension. Enter 0 to return to main menu.\n", (name) => {
+			if(name == "0") recursiveRead(reg);
+			blockApp(reg, count, name);
+			recursiveRead(reg);
+		});
+	} else if (option =="2") {
+		readline.question("Please enter the name of a program you want to unblock, including the .exe extension. Enter 0 to return to main menu.\n", (name) => {
+			if(name == "0") recursiveRead(reg);
+			unblockApp(reg,name);
+			recursiveRead(reg);
+		});
+	}
+}
+
+function blockApp (reg, count, name) {
+	reg.set(count,"REG_SZ",name, function (err){
+		if (err) console.log('ERROR: '+err);
+	});
+}
+
+//Iterates through blocked program list to unblock desired application
+function unblockApp (reg, name) {
+	reg.values(function (err, items) {
+		if (err) console.log(err);
+		items.forEach(element => {
+			if(element.value == name) {
+				reg.remove(element.name, function(err) {
+					if(err) console.log(err);
+				});
+			}
+		});
+	});
+}
 
